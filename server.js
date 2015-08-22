22162bb66493cde394cab0a5209afc2126552995
Jeff Nelson <dj.synthe@gmail.com>
2015-08-21 23:26:23 -0700

    More setup

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var restify = require('restify');
+
+var server = restify.createServer();
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
