66fac3b3f784c9b803ebddd0915ce47ded68637d
Jeff Nelson <dj.synthe@gmail.com>
2015-08-22 12:03:15 -0700

    Made everything work. Adding cli implementation (basic). Some rules updates to linting.

---
+++ cli.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// this is a command line implementation of the acronym library
+// usage:
+//   node --harmony cli fbi --limit=2
+//   --limit is optional
+// limit must be passed before acronyms
+
+var minimist = require('minimist'),
+  acronym = require('./lib/acronym');
+
+var argv = minimist(process.argv.slice(2));
+
+if (!argv._ || !argv._.length) {
+  throw new Error('You have not passed any acronyms to search');
+}
+
+function handleResponse(acro, results) {
+  console.log('Results for: ', acro);
+  results.forEach(item => console.log(' - ', item));
+}
+
+function handleError(acro, err) {
+  console.log('Error for term: ', acro);
+  console.log(' - ', err.message);
+}
+
+for (let acro of argv._) {
+  /*eslint-disable no-loop-func */
+  acronym.request(acro, argv.limit || null)
+    .then(results => handleResponse(acro, results))
+    .catch(err => handleError(acro, err));
+  /*eslint-enable no-loop-func */
+}
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 'use strict';
 
-var restify = require('restify');
+// this is a rest server implementation of the acronym library
+
+var restify = require('restify'),
+  acro = require('./lib/acronym');
 
 var server = restify.createServer();
 
+server.use(restify.queryParser());
+
 function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
+  acro.request(req.params.acronym, req.query.limit || null)
+    .then(function(fullWords) {
+      res.send({results: fullWords});
+      next();
+    }).catch(function(err) {
+      return next(new restify.BadRequestError(err.message || err));
+    });
 }
 
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.get('/:acronym', respond);
+server.head('/:acronym', respond);
 
-server.listen(8080, function() {
+server.listen(8000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
